0061b5126361fade79438a5ab08b238bc8c2dd7b
Gaudeline <gaudeline.d@gmail.com>
2019-11-30 16:09:38 +0100

    pas compris

---
+++ js-exercises-base2/ex03/script.js
@@ -0,0 +1,23 @@
+let a = "3";
+let b = "2";
+function triple(){
+    x = x * 3;
+}
+function affiche(){
+    resulta =
+    resultb =
+    alert(resulta);
+    alert(resultb);
+}
+document.getElementById("affiche").addEventListener("click", () => {
+    affiche();
+});
+
+
+
+// Exercice 3 - écriture d'une fonction avec arguments
+
+// Créez deux variables globales a et b, initialisées respectivement à 3 et à 2.
+// Créez une fonction triple prenant un argument x et renvoyant le résultat de la multiplication de x par 3
+// Créez une fonction affiche, appelée au clic sur un bouton, qui affiche dans des boîtes d'alerte successivement
+//  le résultat de triple appliquée à a, puis à b.
