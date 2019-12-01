4c4c62b7988455430947136c4566796631ba73cc
Gaudeline <gaudeline.d@gmail.com>
2019-12-01 21:03:15 +0100

    à finir

---
+++ js-exercises-base2/ex09/script.js
@@ -0,0 +1,19 @@
+
+document.getElementById('push').addEventListener('click', () => {
+    function testWhile(){
+        saisie=prompt('Entrer une chaîne de caractères contenant la lettre p');
+        alert('"' + saisie + '"');
+        while (saisie =);
+        {
+        saisie=prompt('Entrer une chaîne de caractères contenant la lettre p');
+        }
+    }
+    testWhile();
+});
+
+
+// Exercice 9 - utilisation de la boucle while
+
+// Créer un bouton. Au click de ce bouton, lancer la fonction testWhile. Cette fonction demande à
+// l'utilisateur de saisir une chaîne de caractères contenant la lettre p, et le redemande tant que
+// l'utilisateur ne l'a pas fait. Il affiche ensuite la chaîne saisie entre guillemets
