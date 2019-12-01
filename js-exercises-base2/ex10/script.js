5225abe5973826ada26e84cc211aca906c2bb88f
Gaudeline <gaudeline.d@gmail.com>
2019-12-01 21:43:50 +0100

    à finir

---
+++ js-exercises-base2/ex10/script.js
@@ -0,0 +1,23 @@
+document.getElementById("push").addEventListener("click", () => {
+    function somme(){
+        for (nombre = prompt("entrer un nombre :"); nombre < 3;);
+            
+    }
+    somme();
+
+});
+// Exercice 10 - utilisation de la boucle for
+
+// Créer un bouton. Au click de ce bouton, lancer la fonction somme.
+// Cette fonction demande trois fois à l'utilisateur de saisir un nombre et affiche ensuite la somme de ces trois nombres.
+
+//for (let pas = 0; pas < 5; pas++) {
+    // Ceci sera exécuté 5 fois
+    // À chaque éxécution, la variable "pas" augmentera de 1
+    // Lorsque'elle sera arrivée à 5, le boucle se terminera.
+
+    // console.log('Faire ' + pas + ' pas vers l\'est');
+    // largeur=prompt("Largeur du rectangle?");
+    // longueur=prompt("Longueur du rectangle?");
+    // surface=largeur * longueur;
+    // alert("La surface est de : " + surface);
